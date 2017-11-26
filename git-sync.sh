d9546fab1c65da539a83c8c8bbfe887af62ee81c
Silas S. Brown <ssb22@cam.ac.uk>
2017-11-26 03:01:24 -0600

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -N http://people.ds.cam.ac.uk/ssb22/mwrhome/mwr2ly.y
 wget -N http://people.ds.cam.ac.uk/ssb22/mwrhome/midi-add-depth.py
-git commit -am update && git push
+git commit -am "Update $(echo $(git diff|grep '^--- a/'|sed -e 's,^--- a/,,')|sed -e 's/ /, /g')" && git push
